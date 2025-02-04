348a897ac7157a7eb4e92daed4bea4e0821617a4
Bernadette <blpats>
2025-02-04 14:03:10 +0000

    Fix Netlify build issue

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,19 +1,13 @@
- const serverless = require("serverless-http");
- const express = require("express");
- const path = require("path");
+const serverless = require("serverless-http");
+const express = require("express");
 
- const app = express();
+const app = express();
 
- // Set EJS as view engine
- app.set("views", path.join(__dirname, "../../views")); // Adjust path as needed
- app.set("view engine", "ejs");
+// Serve static files
+app.use(express.static("public"));
 
- // Serve static files
- app.use(express.static(path.join(__dirname, "../../public")));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
- // Define a route
- app.get("/", (req, res) => {
-   res.render("index"); // Renders views/index.ejs
- });
-
- module.exports.handler = serverless(app);
+module.exports.handler = serverless(app);
