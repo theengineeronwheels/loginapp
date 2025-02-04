33bb4144ffd18ef235a4d95850a23e11bd70aa62
Bernadette <blpats>
2025-02-04 15:06:58 +0000

    Fixed server issue

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,13 +1,19 @@
 const serverless = require("serverless-http");
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-// Serve static files
-app.use(express.static("public"));
+// Set up EJS
+app.set("views", path.join(__dirname, "../../views"));
+app.set("view engine", "ejs");
 
+// Serve static assets
+app.use(express.static(path.join(__dirname, "../../public")));
+
+// Define routes
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.ejs");
+  res.render("index"); // Make sure "views/index.ejs" exists
 });
 
 module.exports.handler = serverless(app);
